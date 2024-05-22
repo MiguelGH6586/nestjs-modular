ff25a295b9868015460e88adff7f74446d4a681b
MiguelGH6586 <miguel.agh97@gmail.com>
2024-05-22 17:40:45 -0500

    useValue added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '1234567';
+const API_KEY_PROD = 'PROD1234567';
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV == 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
