731569ab89f90c6c7494af2b33f4a6a6601f9a9a
MiguelGH6586 <miguel.agh97@gmail.com>
2024-05-26 15:33:15 -0500

    update categories to products added

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -55,7 +55,36 @@ export class ProductsService {
       const brand = await this.brandRepo.findOneBy({ id: changes.brandId });
       product.brand = brand;
     }
+    if (changes.categoriesIds) {
+      const categories = await this.categoryRepo.findBy({
+        id: In(changes.categoriesIds),
+      });
+      product.categories = categories;
+    }
     this.productRepo.merge(product, changes);
+    return this.productRepo.save(product);
+  }
+
+  async removeCategoryByProduct(productId: number, categoryId: number) {
+    const product = await this.productRepo.findOne({
+      where: { id: productId },
+      relations: ['categories'],
+    });
+    product.categories = product.categories.filter(
+      (item) => item.id !== categoryId,
+    );
+    return this.productRepo.save(product);
+  }
+
+  async addCategoryToProduct(productId: number, categoryId: number) {
+    const product = await this.productRepo.findOne({
+      where: { id: productId },
+      relations: ['categories'],
+    });
+    const category = await this.categoryRepo.findOne({
+      where: { id: categoryId },
+    });
+    product.categories.push(category);
     return this.productRepo.save(product);
   }
 
